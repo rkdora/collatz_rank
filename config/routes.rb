6ab463cc8f8fba55f62bb1fe085a254da4f46ded
ryuto <rryuto5210@gmail.com>
2019-05-15 19:30:58 +0900

    Add routes users & root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "users#new"
+  resources "users"
 end
