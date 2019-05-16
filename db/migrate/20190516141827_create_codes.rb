e094e5e2416610b395cb9a98dd7ecfd15886fcf1
ryuto <rryuto5210@gmail.com>
2019-05-16 23:19:00 +0900

    rails g model Code user:references content:text

---
+++ db/migrate/20190516141827_create_codes.rb
@@ -0,0 +1,10 @@
+class CreateCodes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :codes do |t|
+      t.references :user, foreign_key: true
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
