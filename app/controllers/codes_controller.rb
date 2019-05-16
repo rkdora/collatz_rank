62c8ed53b3ab0623ca994dd336e8964d381e8b55
ryuto <rryuto5210@gmail.com>
2019-05-16 23:47:25 +0900

    Add action for codes_controller

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,10 +1,28 @@
 class CodesController < ApplicationController
   def index
+    @codes = Code.all
   end
 
   def new
+    @code = Code.new
+  end
+  
+  def create
+    @code = current_user.codes.build(code_params)
+    if @code.save
+      redirect_to @code
+    else
+      render 'new'
+    end
   end
 
   def show
+    @code = Code.find(params[:id])
+  end
+
+  private
+
+  def code_params
+    params.require(:code).permit(:content)
   end
 end
