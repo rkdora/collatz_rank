e975936dc77c8d3cc6333758191e0c42304bd130
ryuto <rryuto5210@gmail.com>
2019-05-16 23:22:50 +0900

    Add null:false maigration file

--- db/migrate/20190516141827_create_codes.rb
+++ db/migrate/20190516141827_create_codes.rb
@@ -1,8 +1,8 @@
 class CreateCodes < ActiveRecord::Migration[5.2]
   def change
     create_table :codes do |t|
-      t.references :user, foreign_key: true
-      t.text :content
+      t.references :user, foreign_key: true, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
