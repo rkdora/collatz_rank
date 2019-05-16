1aad6387ce221a95f6f3d12c4210b81e38f53f97
ryuto <rryuto5210@gmail.com>
2019-05-16 23:29:46 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_103213) do
+ActiveRecord::Schema.define(version: 2019_05_16_141827) do
+
+  create_table "codes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_codes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
