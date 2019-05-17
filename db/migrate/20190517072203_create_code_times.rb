390bb44eec4f90e67eafeace9d10918003948a7e
ryuto <rryuto5210@gmail.com>
2019-05-17 16:25:16 +0900

    Add null: false & rails db:migrate

--- db/migrate/20190517072203_create_code_times.rb
+++ db/migrate/20190517072203_create_code_times.rb
@@ -1,8 +1,8 @@
 class CreateCodeTimes < ActiveRecord::Migration[5.2]
   def change
     create_table :code_times do |t|
-      t.references :code, foreign_key: true
-      t.string :time
+      t.references :code, foreign_key: true, null: false
+      t.string :time, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_141827) do
+ActiveRecord::Schema.define(version: 2019_05_17_072203) do
+
+  create_table "code_times", force: :cascade do |t|
+    t.integer "code_id", null: false
+    t.string "time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code_id"], name: "index_code_times_on_code_id"
+  end
 
   create_table "codes", force: :cascade do |t|
     t.integer "user_id", null: false
