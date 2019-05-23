df372fd6a766992b1efe9634fdc43abd3d29c1ce
ryuto <rryuto5210@gmail.com>
2019-05-23 09:59:46 +0900

    rails db:migrate

--- db/migrate/20190523005833_create_themes.rb
+++ db/migrate/20190523005833_create_themes.rb
@@ -1,7 +1,7 @@
 class CreateThemes < ActiveRecord::Migration[5.2]
   def change
     create_table :themes do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :body
 
       t.timestamps
