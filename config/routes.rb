acef211610097a1fb318c8fe35b53d28182796cc
ryuto <rryuto5210@gmail.com>
2019-05-23 10:17:36 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'themes/index'
-  get 'themes/new'
   root 'times#index'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources 'themes', shallow: true do
+    resources 'codes'
+  end
   resources 'users'
-  resources 'codes'
   resources 'times'
 end
