69387767d45d0ee8c307c526d2d432fdc054eb27
ryuto <rryuto5210@gmail.com>
2019-05-23 10:14:00 +0900

    Add validates

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,5 +1,6 @@
 class Code < ApplicationRecord
   belongs_to :user
+  belongs_to :theme
   has_one :code_time
 
   validates :content, presence: true
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,2 +1,5 @@
 class Theme < ApplicationRecord
+  has_many :codes
+
+  validates :title, presence: true, uniqueness: true
 end
