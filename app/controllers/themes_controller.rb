428ff7f9279c530791918dd8f2f535d6b6124a9a
ryuto <rryuto5210@gmail.com>
2019-05-23 10:23:06 +0900

    Add theme controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,7 +1,28 @@
 class ThemesController < ApplicationController
   def index
+    @themes = Theme.all
   end
 
   def new
+    @theme = Theme.new
+  end
+  
+  def create
+    @theme = Theme.new(theme_params)
+    if @theme.save
+      redirect_to @theme, success: 'テーマを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @theme = Theme.find(params[:id])
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:title, body)
   end
 end
