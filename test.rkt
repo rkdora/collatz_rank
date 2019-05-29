1015ff0e230cc13c28fd9897df5373151e7904cb
ryuto <rryuto5210@gmail.com>
2019-05-29 15:01:12 +0900

    Add indent for code

--- test.rkt
+++ test.rkt
@@ -1,2 +1,42 @@
-    #lang racket
-    (time (apply * (range 1 1000)))
+#lang racket
+
+(define member?
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      (else (or (eq? a (car l))
+                (member? a (cdr l)))))))
+
+(define multirember-aux
+  (lambda (a l ans)
+    (cond
+      ((null? l) ans)
+      ((eq? a (car l)) (multirember-aux a (cdr l) ans))
+      (else
+       (multirember-aux a (cdr l) (cons (car l) ans))))))
+
+(define multirember
+  (lambda (a l)
+    (multirember-aux a l '())))
+
+(define make-set-aux
+  (lambda (l ans)
+    (cond
+      ((null? l) ans)
+      ((member? (car l) ans) (make-set-aux (multirember (car l) l) ans))
+      (else
+       (make-set-aux (multirember (car l) l) (cons (car l) ans))))))
+
+(define make-set
+  (lambda (l)
+    (make-set-aux l '())))
+
+(define test-case
+  (lambda (n)
+    (cond
+      ((zero? n) '())
+      (else (cons (random 10) (test-case (sub1 n)))))))
+
+(define test-case-1000000 (test-case 1000000))
+
+(time (make-set test-case-1000000))
