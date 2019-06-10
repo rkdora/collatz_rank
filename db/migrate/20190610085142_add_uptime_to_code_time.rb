a57cd3c99ded07093e3a2565843fe8591cf265a0
ryuto <rryuto5210@gmail.com>
2019-06-10 17:53:58 +0900

    Add default 0.00 & rails db:migrate

--- db/migrate/20190610085142_add_uptime_to_code_time.rb
+++ db/migrate/20190610085142_add_uptime_to_code_time.rb
@@ -1,5 +1,5 @@
 class AddUptimeToCodeTime < ActiveRecord::Migration[5.2]
   def change
-    add_column :code_times, :uptime, :float
+    add_column :code_times, :uptime, :float, default: 0.00
   end
 end
