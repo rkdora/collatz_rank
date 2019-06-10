907871cd815082d342594d396296899001366774
ryuto <rryuto5210@gmail.com>
2019-06-10 20:19:43 +0900

    Add color theme & new top user

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -13,11 +13,16 @@ class ThemesController < ApplicationController
 
     Theme.all.each do |theme|
       rank = Code.joins(:code_time).eager_load(:code_time).where(theme_id: theme.id).order(:time)
+      new_code = Code.joins(:code_time).eager_load(:code_time).where(theme_id: theme.id).order(:created_at).last
 
       if rank[0].nil?
         @themes.push([theme])
+      elsif (Time.now - rank.first.created_at) < 24.hours && (Time.now - new_code.created_at) < 24.hours
+        @themes.push([theme, rank[0].user, "code", "top"])
       elsif (Time.now - rank.first.created_at) < 24.hours
-        @themes.push([theme, rank[0].user, "new"])
+        @themes.push([theme, rank[0].user, "top"])
+      elsif (Time.now - new_code.created_at) < 24.hours
+        @themes.push([theme, rank[0].user, "code"])
       else
         @themes.push([theme, rank[0].user])
       end
