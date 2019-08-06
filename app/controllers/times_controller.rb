5ea409f6d1453f6f7d40863c97d91309ec6f11d9
ryuto <rryuto5210@gmail.com>
2019-08-06 15:31:16 +0900

    Dir.chdir

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -8,16 +8,18 @@ class TimesController < ApplicationController
     end
     uptime = `uptime`
     uptime = uptime.split.last(3).first.to_f
-    code_out_put = `racket public/uploads/theme_images/test.rkt`
-    cpu_time = code_out_put.match(/time:/).post_match.to_i
-    @code_time = CodeTime.new(code_id: @code.id,
-                              time: cpu_time,
-                              out_put: code_out_put,
-                              uptime: uptime)
-    if code_out_put != "" && @code_time.save
-      redirect_to @code_time.code, success: '計測に成功しました'
-    else
-      redirect_to @code, danger: '計測に失敗しました'
+    Dir.chdir("public/uploads/theme_images/") do
+      code_out_put = `racket test.rkt`
+      cpu_time = code_out_put.match(/time:/).post_match.to_i
+      @code_time = CodeTime.new(code_id: @code.id,
+                                time: cpu_time,
+                                out_put: code_out_put,
+                                uptime: uptime)
+      if code_out_put != "" && @code_time.save
+        redirect_to @code_time.code, success: '計測に成功しました'
+      else
+        redirect_to @code, danger: '計測に失敗しました'
+      end
     end
   end
 end
